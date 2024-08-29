1f414d46c7d6067a76c9da3e592ea1a10813f268
uabdirasulm <ubbiniyazov04@gmail.com>
2024-08-29 09:41:30 +0500

    add editor page

---
+++ app/(root)/documents/[id]/page.tsx
@@ -0,0 +1,17 @@
+import { Editor } from "@/components/editor/Editor";
+import Header from "@/components/Header";
+
+const Document = () => {
+  return (
+    <div>
+      <Header>
+        <div className="flex w-fit items-center justify-center gap-2">
+          <p className="document-title">Fake title</p>
+        </div>
+      </Header>
+      <Editor />
+    </div>
+  );
+};
+
+export default Document;
