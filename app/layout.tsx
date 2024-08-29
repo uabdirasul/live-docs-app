9afe1836c5c6534e00a6fb49a5d221fc02f62719
uabdirasulm <ubbiniyazov04@gmail.com>
2024-08-29 14:38:06 +0500

    add loveblocks auth

--- app/layout.tsx
+++ app/layout.tsx
@@ -4,6 +4,7 @@ import { cn } from "@/lib/utils";
 import { ClerkProvider } from "@clerk/nextjs";
 import { dark } from "@clerk/themes";
 import "./globals.css";
+import Provider from "./Provider";
 
 const fontSans = FontSans({
   subsets: ["latin"],
@@ -34,7 +35,7 @@ export default function RootLayout({
             fontSans.variable
           )}
         >
-          {children}
+          <Provider>{children}</Provider>
         </body>
       </html>
     </ClerkProvider>
