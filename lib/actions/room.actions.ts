d3313e8a1ef5dcffe7cfe4c58900633f7a76d71f
uabdirasulm <ubbiniyazov04@gmail.com>
2024-08-30 16:52:19 +0500

    add editing title

--- lib/actions/room.actions.ts
+++ lib/actions/room.actions.ts
@@ -57,3 +57,18 @@ export const getDocument = async ({
     console.log(`Error occured`, error);
   }
 };
+
+export const updateDocument = async (roomId: string, title: string) => {
+  try {
+    const updatedRoom = await liveblocks.updateRoom(roomId, {
+      metadata: {
+        title
+      }
+    });
+    revalidatePath(`/documents/${roomId}`);
+
+    return parseStringify(updatedRoom);
+  } catch (error) {
+    console.log(error);
+  }
+};
