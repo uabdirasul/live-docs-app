6a36539317743188c90585102f9e4e2e97d9f430
uabdirasulm <ubbiniyazov04@gmail.com>
2024-09-01 18:17:49 +0500

    add delete modal feature

--- components/editor/Editor.tsx
+++ components/editor/Editor.tsx
@@ -21,6 +21,7 @@ import Loader from "../Loader";
 
 import { useThreads } from "@liveblocks/react/suspense";
 import Comments from "../Comments";
+import { DeleteModal } from "../DeleteModal";
 import FloatingToolbarPlugin from "./plugins/FloatingToolbarPlugin";
 
 // Catch any errors that occur during Lexical updates and log them
@@ -57,7 +58,7 @@ export function Editor({
       <div className="editor-container size-full">
         <div className="toolbar-wrapper flex min-w-full justify-between">
           <ToolbarPlugin />
-          {/* {currentUserType === "editor" && <DeleteModal roomId={roomId} />} */}
+          {currentUserType === "editor" && <DeleteModal roomId={roomId} />}
         </div>
 
         <div className="editor-wrapper flex flex-col items-center justify-start">
